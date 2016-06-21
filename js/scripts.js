086ae20d4ee2a610e56ff9f599d6f2d79ea19a96
3vivalagitana3@gmail.com and mr.tamron@gmail.com <Tam Nguyen>
2016-06-20 19:38:14 -0700

    update user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// business logic
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
@@ -6,21 +7,21 @@ var leapYear = function(year) {
   }
  };
 
- // if (year%4 === 0) {
- //   return true;
- // } else if (year%100 === 0) {
- //   return false;
- // } else if (year%400 === 0) {
- //   return true;
- // } else {
- //   return false;
- // }
-
+// user interface logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if(!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
