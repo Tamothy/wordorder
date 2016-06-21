12d5f71bc426618e9041e88d29a9385446ebbfe0
3vivalagitana3@gmail.com and mr.tamron@gmail.com <Jenna Montero>
2016-06-20 20:40:38 -0700

    add BDD specs

---
+++ README.md
@@ -0,0 +1,5 @@
+# behavior     |                   input        |       output
+### don't put anything in    |      nothing             | false
+### put one word in    |            hello               | "hello 1"
+### put two words in     |          hello world     |    "hello 1, world 1"
+### put two of the same words |   hello hello         | "hello 2"
--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,17 @@
 // business logic
-var leapYear = function(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
- };
+var words = function (word) {
+  return false;
+}
+
+
 
 // user interface logic
 $(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
+  $("form#wordorder").submit(function(event) {
     event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
 
-    $(".year").text(year);
+    $(".word").text(words);
 
-    if(!result) {
-      $(".not").text("not");
-    } else {
-      $(".not").text("");
-    }
 
     $("#result").show();
   });
